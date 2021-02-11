196db9cd58fbb5e6d660aa66c97af5c1d535d952
duhaemad <75424796+duhaemad@users.noreply.github.com>
2021-02-11 19:28:40 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files
+app.use(express.static(__dirname + '/dist/monaseb'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res){
+    res.sendFile(path.join(__dirname + '/dist/monaseb/index.html'));
+});
+
+// Heroku PORT
+app.listen(process.env.PORT || 3000);
